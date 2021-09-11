5be60ffc344e4016d68434f69eba464d787584ef
dollis2002 <59921620+dollis2002@users.noreply.github.com>
2021-09-10 20:15:21 -0500

    Added sales class

--- activities/act1_2/sale.h
+++ activities/act1_2/sale.h
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <vector>
 #include "car.h"
 #include "motobike.h"
 #include "suv.h"
+#include "buyer.h"
 using namespace std;
 
 class Sale {
@@ -9,5 +11,26 @@ class Sale {
         vector<Car> soldCars;
         vector<Motorbike> soldBikes;
         vector<Suv> soldSuvs;
-        
+        Buyer client;
+
+    public:
+        Sale();
+
+        void addCar(Car c){
+            soldCars.push_back(c);
+        };
+        void addBike(Motorbike mb){
+            soldBikes.push_back(mb);
+        };
+        void addSuv(Suv s){
+            soldSuvs.push_back(s);
+        };
+        void setClient(Buyer b){
+            client = b;
+        }
+
+        vector<Car> getCarsSold(){ return soldCars; };
+        vector<Motorbike> getBikesSold(){ return soldBikes; };
+        vector<Suv> getSuvsSold(){ return soldSuvs; };
+        Buyer getClient(){ return client; };
 }

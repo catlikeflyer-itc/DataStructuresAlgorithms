51a04ad8616d5a69ad1a3dbebcaed34c202c4db3
dollis2002 <a01025276@itesm.mx>
2021-11-10 14:59:35 -0600

    Added act 4.1

---
+++ activities/act4_1/main.cpp
@@ -0,0 +1,61 @@
+//
+//  main.cpp
+//  actividad_4-1
+//
+//  Created by Vicente Cubells on 13/11/20.
+//
+#include <iostream>
+#include "graph.hpp"
+
+void loadGraph2(int v, int e, Graph<int, int> * graph)
+{
+    /* Implementar */
+}
+
+void loadGraph(int v, int e, std::vector < std::vector<int> > & graph)
+{
+    /* Implementar */
+}
+
+void DFS(std::vector < std::vector<int> > & graph, int u)
+{
+    /* Implementar */
+}
+
+void BFS(Graph<int, int> * graph, int u)
+{
+    /* Implementar */
+}
+
+int main(int argc, const char * argv[]) {
+    
+    /* Definir número de vértices y aristas */
+    const int vertices = 10;
+    const int aristas = 15;
+    
+    /* Declaración del grafo como matriz de adyacencia */
+    std::vector < std::vector<int> > matriz_adyacencia(vertices);
+
+    /* Generar el grafo como matriz de adyacencia */
+    loadGraph(vertices, aristas, matriz_adyacencia);
+    
+    /* Recorrido con DFS */
+    std::cout << "------ Matriz de adyacencia con DFS ------" << std::endl;
+    int u = 1;
+    DFS(matriz_adyacencia, u);
+    
+    /* Declaración del grafo como multilista */
+    Graph<int, int> * multilista = new Graph<int, int>();
+
+    /* Generar el grafo como multilista */
+    loadGraph2(vertices, aristas, multilista);
+    
+    /* Recorrido con BFS */
+    std::cout << "------ Multilista con BFS ------" << std::endl;
+    u = 1;
+    BFS(multilista, u);
+    
+    delete multilista;
+    
+    return 0;
+}

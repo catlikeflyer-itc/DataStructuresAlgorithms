3fd95455a3924c803d4935254225c5008c2af9f7
GlowingMan <cabreraruiz.emi@gmail.com>
2021-11-22 09:05:02 -0600

    Added act5_1
    + main.cpp (has nothing)
    + HashMap.hpp (is just there)
    + HashMap.cpp (modified some functions, but it is still not done)

---
+++ activities/act5_1/HashMap.cpp
@@ -0,0 +1,171 @@
+//
+//  HashMap.cpp
+//  HashMap
+//
+//  Created by Vicente Cubells on 20/11/20.
+//
+
+#include "HashMap.hpp"
+#include <vector>
+#include <stack>
+#include <iostream>
+
+template <class K, class V>
+class HashMap {
+    int _capacity = 0;
+    
+    std::vector<std::stack<K> *> _keys; // vector de stacks que guarda las llaves
+    std::vector<std::stack<V> *>  _values; // vector de stacks que guarda los valores
+    std::vector<int> _status; // vector  que indica en que posicion va el desbordamiento  del vector correspondiente, es decir, cuantos <key,value> se han insertado en un mismo indice. Empieza en 0.
+    
+    int _size = 0;
+    
+    int hash_function(K) const;
+    
+public:
+    HashMap(int);
+    ~HashMap();
+    
+    bool empty() const;
+    int size() const;
+    int capacity() const;
+    
+    bool put(K,V);
+    int rehash(int, int);
+    V get(K);
+    
+    template <typename Kn, typename Vn>
+    friend std::ostream & operator <<(std::ostream & os, const HashMap<Kn,Vn> & hm);
+};
+
+template <class K, class V>
+HashMap<K,V>::HashMap(int capacity)
+{
+    this->_capacity = capacity;
+    this->_status = std::vector<std::string>(capacity);
+    this->_keys = std::vector<K>(capacity);
+    this->_values = std::vector<V>(capacity);
+    
+    fill(_status.begin(), _status.end(), "vacio");
+}
+
+template <class K, class V>
+HashMap<K,V>::~HashMap()
+{
+    /*
+    delete this->_status;
+    delete this->_values;
+    delete this->_keys;
+     */
+}
+
+template <class K, class V>
+bool HashMap<K,V>::empty() const
+{
+    return this->_size == 0;
+}
+
+template <class K, class V>
+int HashMap<K,V>::size() const
+{
+    return this->_size;
+}
+
+template <class K, class V>
+int HashMap<K,V>::capacity() const
+{
+    return this->_capacity;
+}
+
+template <class K, class V>
+int HashMap<K,V>::hash_function(K key) const
+{
+    return key % this->_capacity;
+}
+
+template <class K, class V> // repurpose to make keys and values go into their respective stacks
+bool HashMap<K,V>::put(K key,V value)
+{
+    if (this->_size == this->_capacity) {
+        return false;
+    }
+    
+    int indice = hash_function(key);
+    
+    if (indice > -1) {
+        this->_keys[indice] = key; // insert into the inner, corresponding stack [MISSING]
+        this->_values[indice] = value; // insert into the inner, corresponding stack [MISSING]
+        this->_status[indice]++; // increment specific size value
+        
+        return true;
+    }
+    
+    return false;
+}
+
+/* template <class K, class V>
+int HashMap<K,V>::rehash(int indice, int steps)
+{
+    if (indice < this->_capacity && steps < this->_capacity) {
+        
+        ++indice;
+        
+        if (indice == this->_capacity) indice = 0;
+        
+        if (this->_status[indice] == "ocupado")
+        {
+            indice = rehash(indice, ++steps);
+        }
+        
+        return indice;
+    }
+    else {
+        return -1;
+    }
+} */
+
+template <class K, class V>
+V HashMap<K,V>::get(K key)
+{
+    bool found = false;
+    int steps = 0;
+    
+    int indice = hash_function(key);
+    
+    while (!found && steps < this->_capacity) {
+        if (this->_status[indice] == "ocupado") {
+            if (this->_keys[indice] == key) {
+                found = true;
+            }
+            else {
+                found = false;
+                ++indice;
+                if (indice == this->_capacity) indice = 0;
+            }
+        }
+        else {
+            ++indice;
+            if (indice == this->_capacity) indice = 0;
+        }
+        
+        ++steps;
+    }
+   
+    if (found) return this->_values[indice];
+    else
+        return -1;
+}
+
+template <class K, class V>
+std::ostream & operator <<(std::ostream & os, const HashMap<K,V> & hm)
+{
+    for (int i = 0; i < hm._capacity; ++i) {
+        if (hm._status[i] == "ocupado") {
+            os << hm._keys[i] << " : " << hm._values[i] << std::endl;
+            
+        }
+    }
+    
+    return os;
+}
+
---
+++ activities/act5_1/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  HashMap
+//
+//  Emiliano Cabrera - A01025453
+//  Do Hyun Nam - A01025276
+//
+
+#include <iostream>
+#include "HashMap.cpp"
+
+int main(int argc, const char * argv[]) {
+    
+    int capacity = 5;
+    
+    /* Crear un HashMap */
+    HashMap<int, int> * tabla = new HashMap<int, int>(capacity);
+    
+    /* Insertar elementos */
+    std::cout << tabla->put(1, 1) << std::endl;
+    std::cout << tabla->put(5, 5) << std::endl;
+    std::cout << tabla->put(11, 11) << std::endl;
+    std::cout << tabla->put(15, 15) << std::endl;
+    std::cout << tabla->put(2, 2) << std::endl;
+    std::cout << tabla->put(8, 8) << std::endl;
+    
+    std::cout << tabla->get(5) << std::endl;
+    std::cout << tabla->get(15) << std::endl;
+    std::cout << tabla->get(8) << std::endl;
+    
+    std::cout << *tabla;
+    
+    /* Eliminar la tabla */
+    delete tabla;
+    
+    return 0;
+}
